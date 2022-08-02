703cb281cf550f36ab169bca7b939ec9cb8c7589
Adam <atcolton@tutanota.com>
2022-08-01 20:59:59 -0600

    Added support for one hot input 	modified:   font_renderer.py

--- font_renderer.py
+++ font_renderer.py
@@ -5,11 +5,10 @@ import font_characters
 
 
 class FontRenderer:
-    """
-    res: resolution of each character in the output image
-    """
-
     def __init__(self, res: int = 32, device=torch.device("cpu"), zoom=20):
+        """
+        res: resolution of each character in the output image
+        """
         font_images_path = os.path.join(os.path.dirname(__file__), "font_images/")
         self.font_characters = font_characters.load_font_characters(
             font_images_path, res=res, zoom=zoom
@@ -17,12 +16,14 @@ class FontRenderer:
         self.font_res = res
         self.font_characters = self.font_characters.to(device)
 
-    """
-    string_tensor: A batchsize x height x width integer tensor, where each
-    'pixel' represents the character index, from 0 to 94
-    """
 
     def render(self, string_tensor: torch.Tensor):
+        """
+        string_tensor: A batchsize x height x width integer tensor, where each
+        'pixel' represents the character index, from 0 to 94
+        """
+        if len(string_tensor) > 3 and string_tensor.shape[1] == 95:
+            string_tensor = string_tensor.argmax(dim=1)
         batchsize: int = string_tensor.shape[0]
         width: int = string_tensor.shape[1]
         height: int = string_tensor.shape[2]
