094fe06a1d782b71b8905da32edf1228e670cfa4
Adam <atcolton@tutanota.com>
2022-08-07 11:41:26 -0600

    Added continuous font renderer, along with demonstration

--- demonstrate_renderer.py
+++ demonstrate_renderer.py
@@ -1,7 +1,7 @@
 import torch
 import matplotlib.pyplot as plt
 
-from font_renderer import FontRenderer
+from font_renderer import FontRenderer, ContinuousFontRenderer
 
 
 def testplot(x):
@@ -13,5 +13,13 @@ if __name__ in {"__main__", "__console__"}:
     fr = FontRenderer(res=64)
     input_t = torch.arange(0, 16).reshape(1, 4, 4)
     res = fr.render(input_t)
+
+    testplot(res[0])
+
+    frc = ContinuousFontRenderer(res=16)
+    input_t = torch.zeros(1, 95, 4, 4)
+    input_t[0,32,:,:] = 0.5
+    input_t[0,63,:,:] = 0.5
+    res = frc(input_t)
 
     testplot(res[0])
--- font_renderer.py
+++ font_renderer.py
@@ -17,17 +17,55 @@ class FontRenderer:
         self.font_res = res
         self.font_characters = self.font_characters.to(device)
 
-    """
-    string_tensor: A batchsize x height x width integer tensor, where each
-    'pixel' represents the character index, from 0 to 94
-    """
-
     def render(self, string_tensor: torch.Tensor):
+        """
+        string_tensor: A batchsize x height x width integer tensor, where each
+        'pixel' represents the character index, from 0 to 94
+
+        Alternatively, string_tensor can be a batchsize x 95 x height x width,
+        where each pixel is a one hot encoded vector.
+        """
         batchsize: int = string_tensor.shape[0]
         width: int = string_tensor.shape[1]
         height: int = string_tensor.shape[2]
+        # Shape: batchsize x width x height x font_res x font_res
         characters = self.font_characters[string_tensor]
         characters_batch_by_2d = characters.movedim(2, 3).reshape(
             batchsize, width * self.font_res, height * self.font_res
         )
         return characters_batch_by_2d
+
+    def __call__(self, string_tensor: torch.Tensor):
+        return self.render(string_tensor)
+
+
+class ContinuousFontRenderer(FontRenderer):
+    """
+    At each character position in the output image, characters are a linear
+    combination of the input character vector. This supports rendering input
+    images which have pixels that are not one-hot-encoded.
+
+    Unlike FontRenderer, this renderer is continuous, and supports backprop as it does not use
+    argmax to produce the output image.
+    """
+
+    def render(self, string_tensor: torch.Tensor):
+        """
+        string_tensor: A batchsize x 95 x height x width integer tensor, where each
+        'pixel' represents the character index, from 0 to 94
+        """
+        assert string_tensor.shape[1] == 95
+        assert len(string_tensor.shape) == 4
+
+        batchsize = string_tensor.shape[0]
+        width = string_tensor.shape[2]
+        height = string_tensor.shape[3]
+        # Shape: batchsize x width x height x font_res x font_res
+        rendered_characters = torch.tensordot(
+            string_tensor, self.font_characters, dims=([1], [0])
+        )
+        characters_batch_by_2d = rendered_characters.movedim(2, 3).reshape(
+            batchsize, width * self.font_res, height * self.font_res
+        )
+        return characters_batch_by_2d
+
